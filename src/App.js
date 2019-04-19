e1eb63f9ce51f7f9369095d87f9795842c541dc0
KirbyPatrick <pkirbyinatx@gmail.com>
2019-04-18 23:12:39 -0500

    API call working in the console

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ const API_KEY = "8a06005f089efcf6a9a451deb0dcbab3"
 // const API_ADDRESS = `http://api.brewerydb.com/v2/locations/?key=${API_KEY}`
 
 class App extends Component {
-  getBrewery = async () => {
-    const api_call = await fetch(`http://api.brewerydb.com/v2/locations/?key=${API_KEY}`);
+  getBrewery = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
   }
-  
+
   render() {
     return (
       <div>
         <Title/>
-        <SearchBar/>
+        <SearchBar getBrewery={this.getBrewery} />
         <BreweryCard/>
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return(
-      <div>
-        <input type="text" placeholder="Search..."></input>
-        <button>Enter</button>
-      </div>
+      <form  onSubmit={this.props.getBrewery}>
+        <input type="text" name="brewery" placeholder="Brewery..."></input>
+        <button>Search</button>
+      </form>
     );
   }
 };
