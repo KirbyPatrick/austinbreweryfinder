79b4bc35696a9ae4d54c641ae9f165f5773b1ae8
KirbyPatrick <pkirbyinatx@gmail.com>
2019-04-19 11:12:26 -0500

    successfully mapped API over cards

--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -4,11 +4,13 @@ import '../App.css';
 class BreweryCard extends React.Component {
   render() {
     return(
-      <div className="col-med-3 col-sm-6 mb-5">
+      <div className="col-med-3 col-sm-3 mb-5">
         <div className="card">
           <div className="card-header">
             <h5>Brewery Name</h5>
-            <p>Brewery Info</p>
+            <p>Address...</p>
+            <p>Address...</p>
+            <p>Address...</p>
           </div>
         </div>
       </div>
--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import BreweryCard from './BreweryCard';
 
 class BreweryList extends Component {
+  state= {
+    url: "https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?key=8a06005f089efcf6a9a451deb0dcbab3",
+    brewery: null
+  };
+
+async componentDidMount() {
+  const res = await axios.get(this.state.url)
+  this.setState({brewery: res.data['data']});
+}
   render() {
     return (
-      <div>
-        <BreweryCard/>
-        <BreweryCard/>
-        <BreweryCard/>
-        <BreweryCard/>
-        <BreweryCard/>
-        <BreweryCard/>
-      </div>
+      <React.Fragment>
+        {this.state.brewery ? (
+          <div className="row">
+            {this.state.brewery.map( brewery => (
+              <BreweryCard />
+            ))}
+          </div>
+        ) : (
+          <h1>Brewerys Loading...</h1>
+        )}
+      </React.Fragment>
     );
   }
 }
