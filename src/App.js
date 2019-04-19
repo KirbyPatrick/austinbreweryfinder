1437a332f6ef386465bb12482a1a2d145db07ada
KirbyPatrick <pkirbyinatx@gmail.com>
2019-04-18 22:17:31 -0500

    title complete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 const API_KEY = "8a06005f089efcf6a9a451deb0dcbab3"
 
-const API_ADDRESS = "http://api.brewerydb.com/v2/locations/?key=8a06005f089efcf6a9a451deb0dcbab3"
-//example of address to call random beer.  
+const API_ADDRESS = `http://api.brewerydb.com/v2/locations/?key=${API_KEY}`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            hello world.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Title/>
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Austin Brewery Finder</h1>
+        <p>Discover local breweries near you!</p>
+      </div>
+    );
+  }
+};
+
+export default Title;
