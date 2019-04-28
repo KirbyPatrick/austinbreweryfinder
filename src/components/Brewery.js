91e45272ffeb11b6f117f8d53f1206191abc8cd3
KirbyPatrick <pkirbyinatx@gmail.com>
2019-04-28 12:12:01 -0500

    minor adjustments

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Brewery extends Component {
+  state = {
+    name: '',
+    breweryIndex: '',
+    imageUrl: '',
+
+  }
+
+  async componentDidMount() {
+    const {breweryIndex} = this.props.match.params;
+
+    //Urls for brewery info
+  }
   render() {
     return (
       <div>
